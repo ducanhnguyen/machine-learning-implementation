e9089d9a2046068046a13cc9c1d6347e5daff5f9
Duc Anh Nguyen <ducanhnguyen@Ducs-MacBook-Pro.local>
2019-04-12 09:44:31 +0700

    add new cost function

--- tf/autoencoder.py
+++ tf/autoencoder.py
@@ -1,6 +1,7 @@
 """
 Implementation of AutoEncoder with 1 hidden layer. I try to encode images, and then decode these images to get the original ones.
 """
+
 import matplotlib.pylab as plt
 import numpy as np
 import tensorflow as tf
@@ -11,22 +12,34 @@ import utils
 class AutoEncoder:
     def __init__(self, M):
         """
-
         :param M: Number of units in the hidden layer
         """
         self.M = M
 
-    def fit(self, X, learning_rate=0.01, epoch=100, batch_size=50):
+    def fit(self, X, learning_rate=0.01, epoch=1, batch_size=100):
         N, D = X.shape
         tf_X = tf.placeholder(dtype=tf.float32)
-        self.tf_W1 = tf.Variable(dtype=tf.float32, initial_value=tf.random.normal(shape=(D, self.M), mean=0, stddev=1))
-        self.tf_b1 = tf.Variable(dtype=tf.float32, initial_value=tf.random.normal(shape=(self.M,)))
 
-        self.tf_W2 = tf.Variable(dtype=tf.float32, initial_value=tf.random.normal(shape=(self.M, D), mean=0, stddev=1))
-        self.tf_b2 = tf.Variable(dtype=tf.float32, initial_value=tf.random.normal(shape=(D,)))
+        self.tf_W = tf.Variable(dtype=tf.float32,
+                                initial_value=tf.random.normal(shape=(D, self.M), mean=0,
+                                                               stddev=1 / D))  # xavier initialization
+        self.tf_bh = tf.Variable(dtype=tf.float32, initial_value=tf.random.normal(shape=(self.M,)))
+        self.tf_bo = tf.Variable(dtype=tf.float32, initial_value=tf.random.normal(shape=(D,)))
 
         tf_Xhat = self.forward(tf_X)
-        tf_cost = tf.math.reduce_sum(tf.square(tf_X - tf_Xhat))
+
+        # Two ways to define cost function: (1) least square, or (2) sigmoid cross entropy
+        # Use traditional cross entropy raises an issue if X_hat = 0 or 1
+
+        # tf_cost = tf.math.reduce_sum(tf.square(tf_X - tf_Xhat))
+
+        tf_cost = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                labels=tf_X,
+                logits=self.forwardLogits(tf_X),
+            )
+        )
+
         train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(tf_cost)
 
         with tf.Session() as session:
@@ -56,13 +69,25 @@ class AutoEncoder:
 
             self.plotCost(iterations, costs)
 
-            # show the first image
+            # show images
             Xhat = session.run(tf_Xhat, feed_dict={tf_X: X})
             self.plotComparison(X[0], Xhat[0])
+            self.plotComparison(X[1], Xhat[1])
+            self.plotComparison(X[2], Xhat[2])
+
+    def forwardLogits(self, X):
+        """
+        Use in cost function
+        :param X:
+        :return:
+        """
+        tf_Z = tf.nn.sigmoid(tf.math.add(tf.matmul(a=X, b=self.tf_W), self.tf_bh))
+        Xhat_net = tf.math.add(tf.matmul(a=tf_Z, b=tf.transpose(self.tf_W)), self.tf_bo)
+        return Xhat_net
 
     def forward(self, X):
-        Z = tf.nn.sigmoid(tf.math.add(tf.matmul(a=X, b=self.tf_W1), self.tf_b1))
-        X_hat = tf.nn.sigmoid(tf.math.add(tf.matmul(a=Z, b=self.tf_W2), self.tf_b2))
+        tf_Z = tf.nn.sigmoid(tf.math.add(tf.matmul(a=X, b=self.tf_W), self.tf_bh))
+        X_hat = tf.nn.sigmoid(tf.math.add(tf.matmul(a=tf_Z, b=tf.transpose(self.tf_W)), self.tf_bo))
         return X_hat
 
     def plotComparison(self, x, xhat):
@@ -72,7 +97,6 @@ class AutoEncoder:
         plt.title('Original')
 
         # reconstruction
-
         plt.subplot(1, 2, 2)
         plt.imshow(xhat.reshape(28, 28), cmap='gray')
         plt.title('Reconstructed')
@@ -95,9 +119,9 @@ class AutoEncoder:
 
 
 def main():
-    X, Y = utils.readCsv('../data/digit-recognizer/train.csv', limit=10)
-    ae = AutoEncoder(M=16)
-    ae.fit(X, learning_rate=0.01, epoch=10000, batch_size=50)
+    X, y = utils.readCsv('../data/digit-recognizer/train.csv')
+    ae = AutoEncoder(M=300)
+    ae.fit(X)
 
 
 main()
